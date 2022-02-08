f149a78e905ee2191ad4f7220f55cf21aca866b5
David Valin Cabrera <hola@davidvalin.com>
2022-02-09 00:44:55 +0100

    not used

--- priv/ui/src/state/actions.tsx
+++ priv/ui/src/state/actions.tsx
@@ -4,11 +4,3 @@
 
 export const SET_ERLANG_NODE_NAME = 'SET_ERLANG_NODE_NAME';
 export const SET_NODE_CONTENT = 'SET_NODE_CONTENT';
-
-
-export function setErlangNodeName(payload:any) {
-  return { type: SET_ERLANG_NODE_NAME, payload };
-}
-export function setNodeContent(payload:any) {
-  return { type: SET_NODE_CONTENT, payload };
-}
